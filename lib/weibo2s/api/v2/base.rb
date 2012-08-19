4d87a20d58ca57099f25a701058cde9d1acce53e
cloud <cloudcry@gmail.com>
2012-08-19 23:33:11 +0800

    good example

--- lib/weibo2s/api/v2/base.rb
+++ lib/weibo2s/api/v2/base.rb
@@ -41,7 +41,7 @@ module WeiboOAuth2
                 self.class.class_eval do
                     define_method(name) do |params|
                        method =  api_info['method'] || 'get'
-                       multipart = build_multipart_bodies(params)
+                       multipart = Base.build_multipart_bodies(params)
                        hashie send(method, api_info['url'], :headers => multipart[:headers], :body => multipart[:body])
                     end
                 end
